2b8824fef3de3acd3fcb2d455eff4a7fb66b519c
WeirdoCat <cykelmygmaerkeligmjc@gmail.com>
2019-09-30 13:29:56 +0200

    dab

---
+++ js/kina.js
@@ -0,0 +1,12 @@
+
+var desc = "gratis men du er produktet"
+
+kinaPrice = function (checkoutTime, checkinTime) {
+return 0;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let parkingLot = new parkingLot(desc);
+    new moniter(parkingLot);
+    main(parkingLot);
+});
---
+++ js/moniter.js
@@ -0,0 +1,7 @@
+class moniter {
+    constructor(parkingLot) {
+this.parkinglot = parkinglot;
+this.targets = this.parkinglot.checkedInCars;
+    }
+
+}
